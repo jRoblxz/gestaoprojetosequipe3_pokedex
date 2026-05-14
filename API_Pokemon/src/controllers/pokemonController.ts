3c5002fa775f0052762d0462d29758ac5968719e
jRoblxz <joaoroblez76@gmail.com>
2026-05-14 06:58:07 -0300

    AB#25 Tratamento de erros

--- API_Pokemon/src/controllers/pokemonController.ts
+++ API_Pokemon/src/controllers/pokemonController.ts
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { Request, Response } from 'express';
 import { buscarPokemonsIniciais } from '../services/pokeApiService';
 
@@ -21,4 +22,48 @@ export const getPokemons = async (req: Request, res: Response): Promise<void> =>
         console.error("Erro no PokemonController:", erro);
         res.status(500).json({ success: false, data: [], erro: 'Erro interno' });
     }
+=======
+import { Request, Response } from "express";
+// Ajuste o caminho de importação do seu service conforme a sua pasta
+import { buscarPokemonPorId, buscarPokemonsIniciais  } from "../services/pokeApiService";
+
+export const getPokemons = async (req: Request, res: Response) => {
+  try {
+    const page = parseInt(req.query.page as string) || 1;
+    const search = (req.query.search as string) || "";
+    const type1 = (req.query.type1 as string) || "all"; // Recebe type1
+    const type2 = (req.query.type2 as string) || "all"; // Recebe type2
+    const limit = parseInt(req.query.limit as string) || 18;
+
+    // 2. Enviamos todos os 4 parâmetros para o Service
+    const resultado = await buscarPokemonsIniciais(
+      page,
+      search,
+      type1,
+      type2,
+      limit,
+    );
+
+    // 3. Retornamos o JSON formatado para o Front-end
+    res.json({
+      success: true,
+      data: resultado.dados,
+      current_page: page,
+      last_page: resultado.lastPage,
+    });
+  } catch (error) {
+    console.error("Erro no Controller:", error);
+    res.status(500).json({ success: false, error: "Erro ao buscar pokemons" });
+  }
+};
+
+export const getPokemonById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const data = await buscarPokemonPorId(id);
+    res.json({ success: true, data });
+  } catch (error) {
+    res.status(404).json({ success: false, error: "Pokémon não encontrado" });
+  }
+>>>>>>> Stashed changes
 };
