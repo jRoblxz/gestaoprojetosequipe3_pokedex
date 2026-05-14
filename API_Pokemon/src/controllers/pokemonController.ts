b91d100b354c75c3afe34ab8582a3281640fa399
jRoblxz <joaoroblez76@gmail.com>
2026-05-14 07:01:52 -0300

    AB#25 Tratamento de erros

--- API_Pokemon/src/controllers/pokemonController.ts
+++ API_Pokemon/src/controllers/pokemonController.ts
@@ -1,30 +1,4 @@
-<<<<<<< Updated upstream
-import { Request, Response } from 'express';
-import { buscarPokemonsIniciais } from '../services/pokeApiService';
-
-export const getPokemons = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const page = parseInt(req.query.page as string) || 1;
-        const search = (req.query.search as string) || '';
-        const type = (req.query.type as string) || ''; // <- Captura o tipo aqui
-        
-        // Passa o tipo para o Service
-        const resultado = await buscarPokemonsIniciais(page, search, type);
-
-        res.status(200).json({
-            success: true,
-            data: resultado.dados,
-            current_page: page,
-            last_page: resultado.lastPage
-        });
-        
-    } catch (erro) {
-        console.error("Erro no PokemonController:", erro);
-        res.status(500).json({ success: false, data: [], erro: 'Erro interno' });
-    }
-=======
 import { Request, Response } from "express";
-// Ajuste o caminho de importação do seu service conforme a sua pasta
 import { buscarPokemonPorId, buscarPokemonsIniciais  } from "../services/pokeApiService";
 
 export const getPokemons = async (req: Request, res: Response) => {
@@ -65,5 +39,4 @@ export const getPokemonById = async (req: Request, res: Response) => {
   } catch (error) {
     res.status(404).json({ success: false, error: "Pokémon não encontrado" });
   }
->>>>>>> Stashed changes
 };
